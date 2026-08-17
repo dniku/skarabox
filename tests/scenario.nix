f41c96251dce7ccbaee11cb0f36467e2ee5042f1
dniku <spinner_eardrum721@simplelogin.com>
2026-08-17 15:15:44 +0100

    Test tmpfs installer writable store overlay

--- tests/scenario.nix
+++ tests/scenario.nix
@@ -98,7 +98,7 @@ pkgs.testers.runNixOSTest {
       memorySize = if fullScenario then 4096 else 2048;
       # Installation reads a large closure from the host store over 9p.
       msize = 32 * 1024;
-      writableStoreUseTmpfs = false;
+      writableStoreUseTmpfs = true;
     };
   };
 
